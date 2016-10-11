6152ab7f9a89ae99c1ddcc2d7c44774e06c82925
Ates Goral <ates.g@myplanet.com>
2016-10-11 15:28:52 -0400

    Use Sinon to make timeout test deterministic

--- test/testTimeout.js
+++ test/testTimeout.js
@@ -1,19 +1,30 @@
 const chai = require('chai');
+const sinon = require('sinon');
 const expect = chai.expect;
 
 const timeout = require('../src/timeout');
 
 describe('timeout', () => {
+  let clock = null;
+
+  beforeEach(() => {
+    clock = sinon.useFakeTimers();
+  });
+
+  afterEach(() => {
+    clock.restore();
+  });
+
   describe('.set()', () => {
     describe('when called with a callback and a delay', () => {
-      it('should call the callback after the delay', (done) => {
-        const start = Date.now();
+      it('should call the callback after the delay', () => {
+        const callback = sinon.spy();
 
-        timeout.set(() => {
-          const elapsed = Date.now() - start;
-          expect(elapsed).to.equal(100);
-          done();
-        }, 100);
+        timeout.set(callback, 100);
+
+        clock.tick(100);
+
+        expect(callback).to.have.been.called;
       });
     });
   });
