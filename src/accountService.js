3a96f58edb35be1d6503bac8bd41be588f257e7d
Ates Goral <ates.g@myplanet.com>
2016-10-11 18:12:43 -0400

    Removed -1 case from account service

--- src/accountService.js
+++ src/accountService.js
@@ -3,12 +3,6 @@ function AccountService(db) {
 }
 
 AccountService.prototype.findById = function (accountId, callback) {
-  if (accountId === -1) {
-    return Promise.resolve({
-      id: -1,
-      name: 'Negative One'
-    });
-  }
   return this.db
     .query('account', { id: accountId })
     .then((results) => results[0]);
